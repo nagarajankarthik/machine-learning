fff8f279400f6192a4d918a0e7bd4f05fb5e09ff
nagarajankarthik <nkarthik3092@gmail.com>
2025-01-05 21:42:17 +0800

    feat: Added header files for random forest and decision tree.

---
+++ src/ensemble/decision_tree.h
@@ -0,0 +1,15 @@
+/*
+ * Header file for decision tree class
+ */
+
+#include <string>
+
+using namespace std;
+
+namespace ml {
+
+	class DecisionTree {
+		public:
+			string impurity_method = "gini";
+	};
+}
---
+++ src/ensemble/random_forest.h
@@ -0,0 +1,15 @@
+/*
+ * header file for random forest class
+ */
+
+namespace ml {
+	
+	class RandomForest {
+		public:
+			// Number of trees 
+			int numTrees;
+
+			void RunSingle();
+	};
+}
+
