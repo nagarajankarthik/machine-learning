eff02a080895b38369248ee75dd7ec308df5f19d
nagarajankarthik <nkarthik3092@gmail.com>
2025-01-30 18:01:57 +0800

    feat: Updated list of class variables and functions.

--- src/ensemble/random_forest_classifier.h
+++ src/ensemble/random_forest_classifier.h
@@ -1,13 +1,34 @@
 /*
  * header file for random forest class
  */
+#include "../utils/json.hpp"
+#include <vector>
+
+using namespace std;
 
 namespace ml {
 	
-	class RandomForest {
+	class RandomForestClassifier {
 		public:
+
+			// Data for features
+			vector<vector<double>> features {};
+
+			// Data for outputs
+			vector<vector<double>> outputs {};
+
+
 			// Number of trees 
-			int numTrees;
+			int numberTrees;
+
+			void setParameters(nlohmann::json parameters);
+
+			void fit();
+
+			void predict_proba();
+
+
+
 
 			void RunSingle();
 	};
