aa329ff2bd17d74418a4d793ed357fb24e1aedd6
nagarajankarthik <nkarthik3092@gmail.com>
2025-01-30 09:41:04 +0800

    feat: Added function to read file containing data for features and outputs.

--- src/utils/read_input.cpp
+++ src/utils/read_input.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <unordered_map>
 #include <vector>
@@ -10,25 +11,70 @@ namespace ml
 {
   /**
    * Reads input parameters from a file.
-   * Only json file format is currently supported.
+   * Only json file format is currently supported for 
+   * the file containing user-specified options.
+   *
+   * This class also reads data from a specified file.
+   * The first line of the data file must contain 
+   * two integers specifying the number of columns 
+   * containing outputs. The remaining columns are assumed to 
+   * contain data for features.
    */
   class ReadInput
   {
   public:
-    std::ifstream inp;
-    std::vector<int> integerInputFirst;
 
-    ReadInput(std::string inputFileName) { inp.open(inputFileName); };
-    ~ReadInput() { inp.close(); };
+    ReadInput() { };
+    ~ReadInput() { };
 
-    nlohmann::json_abi_v3_11_3::json readJson()
+    /**
+     *Read input parameters from a file in JSOM format.
+     */
+    nlohmann::json_abi_v3_11_3::json readJson(std::string inputFileName)
     {
 
+      std::ifstream inp;
+      inp.open(inputFileName);
       std::stringstream buffer;
       buffer << inp.rdbuf();
       auto inputParameters = nlohmann::json::parse(buffer.str());
+      inp.close();
       return inputParameters;
     }
+
+    /**
+     * Read data for features and outputs from a user-specified data file
+     * The first line of the file must being with two integers specifying 
+     * the number of columns containing features and outputs respectively.
+     *
+     * @param dataFileName: Absolute path to data file.
+     * @param features: Array to store data for features.
+     * @param outputs: Array to store data for outputs.
+     */
+    void readData(std::string dataFileName, 
+		    std::vector<std::vector<double>> & features, 
+		    std::vector<std::vector<double>> & outputs ) {
+	    std::ifstream inp;
+	    inp.open(dataFileName);
+	    std::string line;
+	    getline(inp, line);
+	    std::stringstream ss(line);
+	    int numberFeatures = 0;
+	    int numberOutputs = 0;
+	    ss >> numberFeatures >> numberOutputs ;
+
+	    while (getline(inp, line)) {
+		    ss << line;
+		    std::vector<double> rowFeatures(numberFeatures, 0.);
+		    std::vector<double> rowOutputs(numberOutputs, 0.);
+		    for (int i = 0; i < numberFeatures; i++) ss >> rowFeatures[i] ;
+		    for (int i = 0; i < numberOutputs; i++) ss >> rowOutputs[i];
+		    features.push_back(rowFeatures);
+		    outputs.push_back(rowOutputs);
+	    }
+
+    }
+
 
     // trim from start (in place)
     inline void ltrim(std::string &s)
