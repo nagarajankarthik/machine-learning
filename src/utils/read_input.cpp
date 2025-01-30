feb4648c780f073fcd268d28c7a402a00da6651f
nagarajankarthik <nkarthik3092@gmail.com>
2025-01-30 18:00:31 +0800

    feat: Removed unnecessary functions.

--- src/utils/read_input.cpp
+++ src/utils/read_input.cpp
@@ -76,54 +76,5 @@ namespace ml
     }
 
 
-    // trim from start (in place)
-    inline void ltrim(std::string &s)
-    {
-      s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch)
-                                      { return !std::isspace(ch); }));
-    }
-
-    // trim from end (in place)
-    inline void rtrim(std::string &s)
-    {
-      s.erase(std::find_if(s.rbegin(), s.rend(),
-                           [](unsigned char ch)
-                           { return !std::isspace(ch); })
-                  .base(),
-              s.end());
-    }
-
-    // trim from both ends (in place)
-    inline void trim(std::string &s)
-    {
-      rtrim(s);
-      ltrim(s);
-    }
-
-    std::unordered_map<std::string, std::string> readOld()
-    {
-      std::string line = "";
-      std::unordered_map<std::string, std::string> params{};
-
-      while (getline(inp, line))
-      {
-        trim(line);
-        if (line[0] == '#' || line.size() == 0)
-          continue;
-
-        // find key value pair from file
-        size_t stopPosition = line.find(':');
-        std::string keyName = line.substr(0, stopPosition);
-
-        // assign value if one is present
-        std::string valueName = "";
-        if (stopPosition + 2 < line.length())
-          valueName = line.substr(stopPosition + 2);
-
-        if (params.find(keyName) == params.end())
-          params[keyName] = valueName;
-      }
-      return params;
-    }
   };
 } // namespace ml
