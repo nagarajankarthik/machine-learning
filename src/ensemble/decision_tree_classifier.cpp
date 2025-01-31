b8ccb9892f42d85dd1a46625a69e9feb2568bdbb
nagarajankarthik <nkarthik3092@gmail.com>
2025-01-31 21:39:18 +0800

    Added destructor.

--- src/ensemble/decision_tree_classifier.cpp
+++ src/ensemble/decision_tree_classifier.cpp
@@ -5,17 +5,24 @@
 using namespace std;
 
 namespace ml{
-DecisionTreeClassifier::DecisionTreeClassifier(nlohmann::json parameters, Logger* _logger) {
+	DecisionTreeClassifier::DecisionTreeClassifier(nlohmann::json parameters, Logger* _logger): logger(_logger) {
 
-	logger = _logger;
+		// Override values of parameters specified in input.
+		if (parameters.contains("impurity_method")) impurity_method = parameters["impurity_method"] ;
+	}
 
-	// Override values of parameters specified in input.
-	if (parameters.contains("impurity_method")) impurity_method = parameters["impurity_method"] ;
-}
+	DecisionTreeClassifier::~DecisionTreeClassifier() {
+		_tree.clear();
+	}
 
-void DecisionTreeClassifier::fit(const vector<vector<double>> & features, const vector<vector<int>> & outputs) {
+	void DecisionTreeClassifier::fit(const vector<vector<double>> & features, const vector<vector<int>> & outputs) {
 
-	return;
+		_tree.clear();
 
-}
+		int number_instances = features.size();
+
+
+		return;
+
+	}
 }
