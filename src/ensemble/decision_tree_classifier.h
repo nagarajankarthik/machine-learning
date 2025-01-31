6222f86fad8847e7f37fdc20a690658270bcd64b
nagarajankarthik <nkarthik3092@gmail.com>
2025-01-31 21:39:01 +0800

    feat: Added class defining attributes of TreeNode.

--- src/ensemble/decision_tree_classifier.h
+++ src/ensemble/decision_tree_classifier.h
@@ -11,11 +11,22 @@ using namespace std;
 
 namespace ml
 {
+	/**
+	 * Class defining attributes of each node in a decision tree.
+	 */
+	struct TreeNode {
+		TreeNode * left_child = nullptr;
+		TreeNode * right_child = nullptr;
+		int feature_split = -1;
+		double value_split = std::numeric_limits<double>::min();
+		vector<int> node_indices {};
+	        int depth = -1;
+		TreeNode(vector<int> _indices, int _depth): node_indices(_indices), depth(_depth) {};
+	};	       
 
 	/**
 	 * Implement decision tree using recursive partitioning algorithm.
 	 */
-
 	class DecisionTreeClassifier
 	{
 	public:
@@ -31,9 +42,19 @@ namespace ml
 		string impurity_method = "gini";
 
 		/**
+		 * Container of TreeNodes.
+		 */
+		vector<TreeNode*> _tree {};
+
+		/**
 		 * Constructor
 		 */
 		DecisionTreeClassifier(nlohmann::json model_parameters, Logger * _logger);
+
+		/**
+		 * Destructor
+		 */
+		~DecisionTreeClassifier() ;
 
 		/**
 		 * Train a decision tree using the recursive partitioning algorithm.
@@ -43,4 +64,5 @@ namespace ml
 
 
 	};
+			
 }
