c283422c5c521206f1f27ddbe104abf7fc1993dd
nagarajankarthik <nkarthik3092@gmail.com>
2025-02-03 07:48:33 +0800

    Updated code to avoid including .cpp files.

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,33 @@
-#include "utils/read_input.cpp"
-#include "utils/misc.cpp"
-#include "utils/evaluate.cpp"
+#include "utils/utils.h"
 #include "ensemble/decision_tree_classifier.h"
 #include "ensemble/random_forest_classifier.h"
-
-#include <string>
-
 
 using namespace std;
 using namespace ml;
 
 int main(int argc, char *argv[])
 {
-    string inputFileName = "input.json";
+    string input_file = "input.json";
     if (argc > 1)
-        inputFileName = argv[1];
-    ReadInput inputReader;
-    auto inputParameters = inputReader.readJson(inputFileName);
+        input_file = argv[1];
 
-    string logFileName = inputParameters["general"]["logfile"];
+    Utilities utils;
+    auto input_parameters = utils.read_json(input_file);
+
+    string log_file = input_parameters["general"]["logfile"];
 
 
     // Create logger instance
-    Logger logger("../logs/" + logFileName);
-    shared_ptr<Logger> logger_ptr = make_shared<Logger>(&logger);
+    shared_ptr<Logger> logger = make_shared<Logger>("../logs/" + log_file);
 
-    nlohmann::json modelSpecifications = inputParameters["models"];
+    nlohmann::json model_specifications = input_parameters["models"];
 
-    // iterate the array
-	for (nlohmann::json::iterator it = modelSpecifications.begin(); it != modelSpecifications.end(); ++it) {
+	for (nlohmann::json::iterator it = model_specifications.begin(); it != model_specifications.end(); ++it) {
 		nlohmann::json model_parameters = *it;
 		string data_path = model_parameters["data"];
 		vector<vector<double>> features {};
 		vector<vector<double>> outputs {};
-		inputReader.readData(data_path, features, outputs);
+		utils.read_data(data_path, features, outputs);
 	        vector<vector<double>> train_features {};	
 	        vector<vector<double>> test_features {};
 		vector<vector<double>> train_outputs {};	
@@ -43,27 +37,27 @@ int main(int argc, char *argv[])
 		if (model_parameters.contains("shuffle_data")) shuffle_data = model_parameters["shuffle_data"];
 		if (model_parameters.contains("train_ratio")) train_ratio = model_parameters["train_ratio"];
 		// train-test split
-		train_test_split(features, outputs, train_features, train_outputs, test_features, test_outputs, train_ratio, shuffle_data, logger_ptr);
+		utils.train_test_split(features, outputs, train_features, train_outputs, test_features, test_outputs, train_ratio, shuffle_data, logger);
 		// type conversion of outputs for classification algorithms	
-		vector<vector<int>> train_outputs_int = double_to_int(train_outputs) ;
-		vector<vector<int>> test_outputs_int = double_to_int(test_outputs) ;
+		vector<vector<int>> train_outputs_int = utils.double_to_int(train_outputs) ;
+		vector<vector<int>> test_outputs_int = utils.double_to_int(test_outputs) ;
 		string model_type = model_parameters["model"];
 		if (model_type == "decision_tree_classifier") {
-			DecisionTreeClassifier decision_tree(model_parameters, &logger);
-			logger.log(INFO, "Training " + model_type);
+			DecisionTreeClassifier decision_tree(model_parameters, logger);
+			logger->log(INFO, "Training " + model_type);
 			decision_tree.fit(train_features, train_outputs_int);
 			vector<vector<int>> predictions = decision_tree.predict(train_outputs_int, test_features);
-			vector<unordered_set<int>> unique_classes = get_unique_classes(train_outputs_int);
-			vector<unordered_set<int>> unique_classes_test = get_unique_classes(test_outputs_int);
+			vector<unordered_set<int>> unique_classes = utils.get_unique_classes(train_outputs_int);
+			vector<unordered_set<int>> unique_classes_test = utils.get_unique_classes(test_outputs_int);
 			for (int i = 0; i < unique_classes.size(); i++) unique_classes[i].insert(unique_classes_test[i].begin(), unique_classes_test[i].end());
-			vector<vector<int>> confusion_matrix = get_confusion_matrix(predictions, test_outputs_int, unique_classes[0], 0);
-			logger.log(INFO, "Confusion matrix for decision tree classifier");
-			logger.log(INFO,"Data file used was " + data_path);
-			logger.log(INFO, array_2d_to_string(confusion_matrix));
+			vector<vector<int>> confusion_matrix = utils.get_confusion_matrix(predictions, test_outputs_int, unique_classes[0], 0);
+			logger->log(INFO, "Confusion matrix for decision tree classifier");
+			logger->log(INFO,"Data file used was " + data_path);
+			logger->log(INFO, utils.array_2d_to_string(confusion_matrix));
 		} else if (model_type == "neural_network_regressor") {
-			logger.log(INFO, "Neural network is not currently implemented but will be coming soon :).");
+			logger->log(INFO, "Neural network is not currently implemented but will be coming soon :).");
 		} else {
-			logger.log(ERROR, "The specified model type is currently unsupported.");
+			logger->log(ERROR, "The specified model type is currently unsupported.");
 		}
 
 	}
