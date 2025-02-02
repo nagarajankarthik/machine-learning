16d0c2087c7dc4f5933f3e159477699eca53ceae
nagarajankarthik <nkarthik3092@gmail.com>
2025-02-02 15:39:01 +0800

    Added function to convert 2D array to string for logging purposes.

--- src/utils/misc.cpp
+++ src/utils/misc.cpp
@@ -2,16 +2,18 @@
 #include <vector>
 #include <chrono>
 #include <random>
+#include <memory>
+#include <unordered_set>
 #include "logging.cpp"
 
 using namespace std;
 
 namespace ml {
 
-/** Split the input data for features and outputs into trainigna nd test sets.
+/** Split the input data for features and outputs into training and test sets.
  *
  */
-void train_test_split(const vector<vector<double>> & features, const vector<vector<double>> & outputs, vector<vector<double>> & train_features, vector<vector<double>> & train_outputs,vector<vector<double>> & test_features,vector<vector<double>> & test_outputs, double train_ratio, bool shuffle_data, Logger * logger) {
+void train_test_split(const vector<vector<double>> & features, const vector<vector<double>> & outputs, vector<vector<double>> & train_features, vector<vector<double>> & train_outputs,vector<vector<double>> & test_features,vector<vector<double>> & test_outputs, double train_ratio, bool shuffle_data, shared_ptr<Logger> logger) {
 
 	if (features.size() != outputs.size()) {
 		logger->log(ERROR, "Features and Outputs datasets have different numbers of records.") ;
@@ -63,5 +65,43 @@ vector<vector<int>> double_to_int(const vector<vector<double>> & data) {
 	 }
 	 return data_int;
 }
+
+/**
+ * Convert input 2D array into a single string.
+ */
+template <class T>
+string array_2d_to_string(vector<vector<T>> matrix) {
+
+	string result("");		
+	int m = matrix.size();
+	int n = matrix[0].size();
+	for (int i = 0; i < m; i++) {
+		string row = "";
+		for (int j = 0; j < n; j++) {
+			row += to_string(matrix[i][j]) + ", ";
+		}
+		result += row + "\n";
+	}
+	return result;
+}
+
+vector<unordered_set<int>> get_unique_classes(vector<vector<int>> outputs) {
+
+	int number_outputs = outputs[0].size();
+
+	unordered_set<int> tmp {};
+
+	vector<unordered_set<int>> unique_classes(number_outputs, tmp);
+
+	for (int i = 0; i < outputs.size(); i++) {
+		for (int j = 0; j < outputs[i].size(); j++) {
+			int current_class = outputs[i][j];
+			unique_classes[j].insert(current_class);
+		}
+	}
+	return unique_classes;
+}
+				
+
 
 }
