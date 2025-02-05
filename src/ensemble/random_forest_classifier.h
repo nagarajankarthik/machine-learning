08d9b076c12aad1065f5660f3040aa64778784db
nagarajankarthik <nkarthik3092@gmail.com>
2025-02-05 11:54:09 +0800

    Added list of class members.

--- src/ensemble/random_forest_classifier.h
+++ src/ensemble/random_forest_classifier.h
@@ -1,8 +1,7 @@
 /*
  * header file for random forest class
  */
-#include "../utils/json.hpp"
-#include <vector>
+#include "decision_tree_classifier.h"
 
 using namespace std;
 
@@ -12,9 +11,39 @@ namespace ml
 	class RandomForestClassifier
 	{
 	public:
-		// Number of trees
-		int numberTrees;
 
-		void fit();
+		/**
+		 * Pointer to an instance of Logger
+		 */
+		shared_ptr<Logger> logger;
+
+		/**
+		 * Parameters to pass to Decision Tree constructor
+		 */
+		nlohmann::json parameters ;
+
+		// Number of trees
+		int number_trees = 20;
+
+		/**
+		 * An array of decision trees
+		 */
+		vector<shared_ptr<DecisionTreeClassifier>> trees {};
+
+		/**
+		 * Constructor
+		 */
+		RandomForestClassifier(nlohmann::json parameters, shared_ptr<Logger> logger)  ;
+
+		/**
+		 * Destructor
+		 */
+		~RandomForestClassifier() {};
+
+		/**
+		 * Perform model training.
+		 */
+		void fit(const vector<vector<double>> & features, const vector<vector<int>> & outputs) ;
 	};
+	
 }
