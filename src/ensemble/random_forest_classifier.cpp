36a02160300f13070ab01df1ad80b8283cc4ddf1
nagarajankarthik <nkarthik3092@gmail.com>
2025-02-05 11:54:39 +0800

    Added implementation of constructor.

--- src/ensemble/random_forest_classifier.cpp
+++ src/ensemble/random_forest_classifier.cpp
@@ -2,6 +2,26 @@
 
 namespace ml
 {
-    void RandomForestClassifier::fit() {}
+	
+	RandomForestClassifier::RandomForestClassifier(nlohmann::json parameters, shared_ptr<Logger> logger): parameters(parameters), logger(logger) {
+	
+		if (parameters.contains("number_trees")) {
+				int number_trees_input = parameters["number_trees"];
+				if (number_trees_input > 0) number_trees = number_trees_input;
+	}
+} 
+    void RandomForestClassifier::fit(const vector<vector<double>> & features, const vector<vector<int>> & outputs) {
+
+	    trees.clear();
+
+	    vector<int> data_indices {};
+
+
+	    for (int i = 0; i < number_trees; i++) {
+		    int tr = 2;
+	    }
+    
+    
+    }
 
 }
