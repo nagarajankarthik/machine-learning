c5ae1b1473479464310875391c4339bd6aafacae
nagarajankarthik <nkarthik3092@gmail.com>
2025-02-19 17:46:55 +0800

    Added first draft of header file for Tensor class.

---
+++ src/computational_graph/tensor.h
@@ -0,0 +1,59 @@
+#include "../utils/logging.h"
+#include <functional>
+#include <vector>
+#include <memory>
+using namespace std;
+
+namespace ml {
+	class Tensor {
+
+		public:
+			/**
+			 * Values contained in tensor
+			 */
+			vector<double> values = {} ;
+
+			/**
+			 * Gradients of loss with respect to tensor
+			 */
+			vector<double> gradients = {};
+
+			/**
+			 * Shape of Tensor
+			 */
+			vector<int> shape = {};
+
+			/**
+			 * Constructor
+			 */
+			Tensor() {} ;
+
+			/**
+			 * Destructor
+			 */
+			~Tensor() {} ;
+
+			/**
+			 * Function to support element indexing.
+			 */
+			double at(vector<int> position) ;
+
+			/**
+			 * Reshape
+			 */
+			void reshape(vector<int> new_shape) ;
+
+			/**
+			 * Back-propagate gradients to input Tensors
+			 */
+			void backward(const vector<double> & seed) ;
+		private:
+			/**
+			 * Pointer to gradient function
+			 */
+			std::function<void(const vector<double>, shared_ptr<Tensor>, shared_ptr<Tensor>)> backward_function ;
+
+	};
+}// namespace ml
+
+
