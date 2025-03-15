2e54cdb3857ea19c8d010cd8b5862b7f64f63d77
nagarajankarthik <nkarthik3092@gmail.com>
2025-03-15 17:24:17 +0800

    Added main test file to run all registered tests.

---
+++ src/test_main.cpp
@@ -0,0 +1,8 @@
+#include <glog/logging.h>
+#include <gmock/gmock.h>
+
+int main(int argc, char **argv) {
+	google::InitGoogleLogging(argv[0]);
+	testing::InitGoogleMock(&argc, argv);
+	return RUN_ALL_TESTS();
+}
