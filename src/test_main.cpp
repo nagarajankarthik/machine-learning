798d927aa5273b66ccd4c745d457de90c1b36ae3
nagarajankarthik <nkarthik3092@gmail.com>
2025-03-15 21:13:44 +0800

    Revert "Added main test file to run all registered tests."
    This reverts commit 2e54cdb3857ea19c8d010cd8b5862b7f64f63d77.

--- src/test_main.cpp
+++
@@ -1,8 +0,0 @@
-#include <glog/logging.h>
-#include <gmock/gmock.h>
-
-int main(int argc, char **argv) {
-	google::InitGoogleLogging(argv[0]);
-	testing::InitGoogleMock(&argc, argv);
-	return RUN_ALL_TESTS();
-}
