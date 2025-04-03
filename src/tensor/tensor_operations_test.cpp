be4664f1c07d77577ed663ae58d6631c5f2d1fdf
nagarajankarthik <nkarthik3092@gmail.com>
2025-04-03 12:01:36 +0800

    Added unit tests for matrix addition and corresponding back-propagation function.

--- src/tensor/tensor_operations_test.cpp
+++ src/tensor/tensor_operations_test.cpp
@@ -26,6 +26,49 @@ class TensorOpsTest : public testing::Test {
 		b = make_shared<Tensor>(values, shape, logger);
 	}
 };
+TEST_F(TensorOpsTest, AddForwardTest) {
+	shared_ptr<Tensor> c = add_batch_forward(a, b);
+	logger->log(INFO, "Created tensor c by adding a and b.");
+	vector<int> position{0};
+	vector<vector<double>> matrix = c->get_matrix(position, "values");
+	for (int i = 0; i < matrix.size(); i++) {
+		for (int j = 0; j < matrix[0].size(); j++) {
+			ASSERT_FLOAT_EQ(matrix[i][j], 2.);
+		}
+	}
+}
+
+TEST_F(TensorOpsTest, AddBackwardTest) {
+	shared_ptr<Tensor> c = add_batch_forward(a, b);
+	logger->log(INFO, "Created tensor c by adding a and b.");
+	fill(c->gradients.begin(), c->gradients.end(), 0.);
+	fill(c->gradients.begin() + 4, c->gradients.end(), 1.);
+	c->backward();
+
+	vector<int> position{0};
+	vector<vector<double>> grad = a->get_matrix(position, "gradients");
+	for (int i = 0; i < grad.size(); i++) {
+		for (int j = 0; j < grad[0].size(); j++) {
+			ASSERT_FLOAT_EQ(grad[i][j], 1.);
+		}
+	}
+
+	grad = b->get_matrix(position, "gradients");
+	for (int i = 0; i < grad.size(); i++) {
+		for (int j = 0; j < grad[0].size(); j++) {
+			ASSERT_FLOAT_EQ(grad[i][j], 0.);
+		}
+	}
+
+	position[0] = 1;
+
+	grad = b->get_matrix(position, "gradients");
+	for (int i = 0; i < grad.size(); i++) {
+		for (int j = 0; j < grad[0].size(); j++) {
+			ASSERT_FLOAT_EQ(grad[i][j], 1.);
+		}
+	}
+}
 
 TEST_F(TensorOpsTest, MatmulForwardTest) {
 	shared_ptr<Tensor> c = batch_matmul_forward(a, b);
