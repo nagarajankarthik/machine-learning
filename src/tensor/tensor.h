da5f3d3cb9f4122819cc20c9988f304fdc5738d5
nagarajankarthik <nkarthik3092@gmail.com>
2025-05-17 18:02:41 +0800

    Added funcion to set values of individual elements.

--- src/tensor/tensor.h
+++ src/tensor/tensor.h
@@ -6,117 +6,122 @@
 #pragma once
 using namespace std;
 
-namespace ml {
-	class Tensor: public enable_shared_from_this<Tensor> {
+namespace ml
+{
+	class Tensor : public enable_shared_from_this<Tensor>
+	{
 
-		public:
-			/**
-			 * Pointer to logger
-			 */
-			shared_ptr<Logger> logger = nullptr;
+	public:
+		/**
+		 * Pointer to logger
+		 */
+		shared_ptr<Logger> logger = nullptr;
 
-			/**
-			 * Values contained in tensor
-			 */
-			vector<double> values = {} ;
+		/**
+		 * Values contained in tensor
+		 */
+		vector<double> values = {};
 
-			/**
-			 * Gradients of loss with respect to tensor
-			 */
-			vector<double> gradients = {};
+		/**
+		 * Gradients of loss with respect to tensor
+		 */
+		vector<double> gradients = {};
 
-			/**
-			 * Shape of Tensor
-			 */
-			vector<int> shape = {};
+		/**
+		 * Shape of Tensor
+		 */
+		vector<int> shape = {};
 
-			/**
-			 * Predecessor in computational graph
-			 */
-			shared_ptr<Tensor> input_first = nullptr;
+		/**
+		 * Predecessor in computational graph
+		 */
+		shared_ptr<Tensor> input_first = nullptr;
 
-			/**
-			 * Predecessor in computational graph
-			 */
-			shared_ptr<Tensor> input_second = nullptr;
+		/**
+		 * Predecessor in computational graph
+		 */
+		shared_ptr<Tensor> input_second = nullptr;
 
-			/**
-			 * Constructor
-			 */
-			Tensor() {} ;
-			
-			/**
-			 * Constructor to assign values
-			 */
-			Tensor(vector<double> values, vector<int> shape, shared_ptr<Logger> logger); ;
+		/**
+		 * Pointer to gradient function
+		 */
+		std::function<void(shared_ptr<Tensor>)> backward_function;
 
-			/**
-			 * Constructor to assign values and inputs
-			 */
-			Tensor(vector<double> values, vector<int> shape, shared_ptr<Logger> logger, shared_ptr<Tensor> input_first, shared_ptr<Tensor> input_second);
+		/**
+		 * Constructor
+		 */
+		Tensor() {};
 
-			/**
-			 * Constructor to assign values, inputs and backward function
-			 */
-			Tensor(vector<double> values, vector<int> shape, shared_ptr<Logger> logger, shared_ptr<Tensor> input_first, shared_ptr<Tensor> input_second, function<void(shared_ptr<Tensor>)> backward_function);
+		/**
+		 * Constructor to assign values
+		 */
+		Tensor(vector<double> values, vector<int> shape, shared_ptr<Logger> logger);
+		;
 
-			/**
-			 * Destructor
-			 */
-			~Tensor() {} ;
+		/**
+		 * Constructor to assign values and inputs
+		 */
+		Tensor(vector<double> values, vector<int> shape, shared_ptr<Logger> logger, shared_ptr<Tensor> input_first, shared_ptr<Tensor> input_second);
 
-			/**
-			 * Function to support element indexing.
-			 */
-			double get_element(vector<int> position) ;
+		/**
+		 * Constructor to assign values, inputs and backward function
+		 */
+		Tensor(vector<double> values, vector<int> shape, shared_ptr<Logger> logger, shared_ptr<Tensor> input_first, shared_ptr<Tensor> input_second, function<void(shared_ptr<Tensor>)> backward_function);
 
-			/**
-			 * Function to retrieve matrix based on specified indices into 
-			 * batch (non-matrix) dimensions. All dimensions except for the last two 
-			 * are considered to be batch dimensions.
-			 */
-			vector<vector<double>> get_matrix(vector<int> position, string item = "values") const ;
+		/**
+		 * Destructor
+		 */
+		~Tensor() {};
 
-			/**
-			 * Function to set a matrix contained within the Tensor based on specified
-			 * indices into 
-			 * batch (non-matrix) dimensions. All dimensions except for the last two 
-			 * are considered to be batch dimensions.
-			 */
-			void set_matrix(vector<int> position, const vector<vector<double>>& matrix, string item = "values") ;
+		/**
+		 * Function to support element indexing.
+		 */
+		double get_element(vector<int> position);
 
-			/**
-			 * Reshape
-			 */
-			void reshape(vector<int> new_shape) ;
+		/**
+		 * Function to set values of individual elements.
+		 */
+		void set_element(vector<int> position, double new_value);
 
-			/**
-			 * Back-propagate gradients to input Tensors
-			 */
-			void backward() ;
-		private:
+		/**
+		 * Function to retrieve matrix based on specified indices into
+		 * batch (non-matrix) dimensions. All dimensions except for the last two
+		 * are considered to be batch dimensions.
+		 */
+		vector<vector<double>> get_matrix(vector<int> position, string item = "values") const;
 
-			/**
-			 * Used to support multi-dimensional indexing
-			 */
-			vector<int> strides = {};
+		/**
+		 * Function to set a matrix contained within the Tensor based on specified
+		 * indices into
+		 * batch (non-matrix) dimensions. All dimensions except for the last two
+		 * are considered to be batch dimensions.
+		 */
+		void set_matrix(vector<int> position, const vector<vector<double>> &matrix, string item = "values");
 
-			/**
-			 * Update strides whenever shape of Tensor changes
-			 */
-			void update_strides() ;
+		/**
+		 * Reshape
+		 */
+		void reshape(vector<int> new_shape);
 
-			/**
-			 * Update indices to account for broadcasting
-			 */
-			vector<int> broadcast_indices(vector<int> position) const ;
+		/**
+		 * Back-propagate gradients to input Tensors
+		 */
+		void backward();
 
-			/**
-			 * Pointer to gradient function
-			 */
-			std::function<void(shared_ptr<Tensor>)> backward_function ;
+	private:
+		/**
+		 * Used to support multi-dimensional indexing
+		 */
+		vector<int> strides = {};
 
+		/**
+		 * Update strides whenever shape of Tensor changes
+		 */
+		void update_strides();
+
+		/**
+		 * Update indices to account for broadcasting
+		 */
+		vector<int> broadcast_indices(vector<int> position) const;
 	};
-}// namespace ml
-
-
+} // namespace ml
