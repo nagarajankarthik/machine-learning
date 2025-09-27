632af0c12c220ea7f479b12ad753086f62ec3c5e
nagarajankarthik <nkarthik3092@gmail.com>
2025-09-27 16:50:09 +0800

    Update utilities class to allow for train and test data to be stored in separate text files.

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -1,126 +1,125 @@
 #include "utils.h"
-#include "json.hpp"
 
-namespace ml
-{
+namespace ml {
 
+Utilities::Utilities(shared_ptr<Logger> logger) : logger(logger) {};
 
-	Utilities::Utilities(shared_ptr<Logger> logger):logger(logger) {} ;	
+void Utilities::train_test_split(const vector<vector<double>> &features,
+                                 const vector<vector<double>> &outputs,
+                                 TrainTestData &train_test) {
 
+  if (features.size() != outputs.size()) {
+    logger->log(
+        ERROR,
+        "Features and Outputs datasets have different numbers of records.");
+    exit(EXIT_FAILURE);
+  }
 
-	TrainTestData Utilities::train_test_split(const vector<vector<double>> &features, const vector<vector<double>> &outputs)
-	{
+  int total_instances = features.size();
+  int number_train = round(train_ratio * total_instances);
+  int number_test = total_instances - number_train;
 
-		if (features.size() != outputs.size())
-		{
-			logger->log(ERROR, "Features and Outputs datasets have different numbers of records.");
-			exit(EXIT_FAILURE);
-		}
+  if (number_train < 1) {
+    logger->log(ERROR, "Training data must have at least one instance.");
+    exit(EXIT_FAILURE);
+  }
 
-		int total_instances = features.size();
-		int number_train = round(train_ratio * total_instances);
-		int number_test = total_instances - number_train;
+  vector<int> data_indices{};
+  for (int i = 0; i < total_instances; i++)
+    data_indices.push_back(i);
 
-		if (number_train < 1)
-		{
-			logger->log(ERROR, "Training data must have at least one instance.");
-			exit(EXIT_FAILURE);
-		}
+  if (shuffle_data)
+    std::shuffle(data_indices.begin(), data_indices.end(),
+                 std::default_random_engine(random_seed));
 
-		vector<int> data_indices{};
-		for (int i = 0; i < total_instances; i++)
-			data_indices.push_back(i);
+  for (int i = 0; i < number_train; i++) {
+    train_test.train_features.push_back(features[data_indices[i]]);
+    train_test.train_labels.push_back(outputs[data_indices[i]]);
+  }
 
-		if (shuffle_data)
-			std::shuffle(data_indices.begin(), data_indices.end(), std::default_random_engine(random_seed));
+  for (int i = 0; i < number_test; i++) {
+    train_test.test_features.push_back(
+        features[data_indices[i + number_train]]);
+    train_test.test_labels.push_back(outputs[data_indices[i + number_train]]);
+  }
+}
 
-		TrainTestData train_test ;
+TrainTestData Utilities::get_train_test_data(nlohmann::json model_parameters) {
 
-		for (int i = 0; i < number_train; i++)
-		{
-			train_test.train_features.push_back(features[data_indices[i]]);
-			train_test.train_labels.push_back(outputs[data_indices[i]]);
-		}
+  TrainTestData train_test{};
 
-		for (int i = 0; i < number_test; i++)
-		{
-			train_test.test_features.push_back(features[data_indices[i + number_train]]);
-			train_test.test_labels.push_back(outputs[data_indices[i + number_train]]);
-		}
+  if (model_parameters.contains("data")) {
+    string data_path = model_parameters["data"];
+    vector<vector<double>> features{};
+    vector<vector<double>> outputs{};
+    read_data(data_path, features, outputs);
+    if (model_parameters.contains("shuffle_data"))
+      shuffle_data = model_parameters["shuffle_data"];
+    if (model_parameters.contains("train_ratio"))
+      train_ratio = model_parameters["train_ratio"];
+    if (model_parameters.contains("random_seed"))
+      random_seed = model_parameters["random_seed"];
+    train_test_split(features, outputs, train_test);
+  } else if (model_parameters.contains("train_data") &&
+             model_parameters.contains("test_data")) {
+    string train_data_path = model_parameters["train_data"];
+    string test_data_path = model_parameters["test_data"];
+    read_data(train_data_path, train_test.train_features,
+              train_test.train_labels);
+    read_data(test_data_path, train_test.test_features, train_test.test_labels);
+  }
+}
 
-		return train_test;
-	}
+nlohmann::json_abi_v3_11_3::json Utilities::read_json(std::string input_file) {
 
+  std::ifstream inp;
+  inp.open(input_file);
+  std::stringstream buffer;
+  buffer << inp.rdbuf();
+  auto inputParameters = nlohmann::json::parse(buffer.str());
+  inp.close();
+  return inputParameters;
+}
 
-	TrainTestData Utilities::get_train_test_data(nlohmann::json model_parameters) {
-		string data_path = model_parameters["data"];
-		int random_seed = model_parameters["random_seed"];
-		vector<vector<double>> features {};
-		vector<vector<double>> outputs {};
-		read_data(data_path, features, outputs);
-		if (model_parameters.contains("shuffle_data")) shuffle_data = model_parameters["shuffle_data"];
-		if (model_parameters.contains("train_ratio")) train_ratio = model_parameters["train_ratio"];
-		if (model_parameters.contains("random_seed")) random_seed = model_parameters["random_seed"];
-		// train-test split
-		TrainTestData train_test = train_test_split(features, outputs);
+void Utilities::read_data(std::string data_file,
+                          std::vector<std::vector<double>> &features,
+                          std::vector<std::vector<double>> &outputs,
+                          char delimiter) {
+  std::ifstream inp;
+  inp.open(data_file);
+  std::string line, value;
+  getline(inp, line);
+  std::stringstream ss(line);
+  int number_features = 0;
+  int number_outputs = 0;
+  getline(ss, value, delimiter);
+  number_features = std::stoi(value);
+  getline(ss, value, delimiter);
+  number_outputs = std::stoi(value);
 
-		return train_test;
-	}
+  logger->log(INFO, "Number of features = " + to_string(number_features));
+  logger->log(INFO, "Number of outputs = " + to_string(number_outputs));
 
+  while (getline(inp, line)) {
+    std::stringstream ssd(line);
+    std::vector<double> row_features(number_features, 0.0);
+    std::vector<double> row_outputs(number_outputs, 0.0);
+    for (int i = 0; i < number_features; i++) {
+      getline(ssd, value, delimiter);
+      row_features[i] = std::stod(value);
+    }
+    for (int i = 0; i < number_outputs; i++) {
+      getline(ssd, value, delimiter);
+      row_outputs[i] = std::stod(value);
+    }
 
-	nlohmann::json_abi_v3_11_3::json Utilities::read_json(std::string input_file)
-	{
+    features.push_back(row_features);
+    outputs.push_back(row_outputs);
+  }
 
-		std::ifstream inp;
-		inp.open(input_file);
-		std::stringstream buffer;
-		buffer << inp.rdbuf();
-		auto inputParameters = nlohmann::json::parse(buffer.str());
-		inp.close();
-		return inputParameters;
-	}
+  inp.close();
 
-	void Utilities::read_data(std::string data_file,
-							  std::vector<std::vector<double>> &features,
-							  std::vector<std::vector<double>> &outputs, char delimiter)
-	{
-		std::ifstream inp;
-		inp.open(data_file);
-		std::string line, value;
-		getline(inp, line);
-		std::stringstream ss(line);
-		int number_features = 0;
-		int number_outputs = 0;
-		getline(ss, value, delimiter);
-		number_features = std::stoi(value);
-		getline(ss, value, delimiter);
-		number_outputs = std::stoi(value);
-		
-		logger->log(INFO, "Number of features = " + to_string(number_features));
-		logger->log(INFO, "Number of outputs = " + to_string(number_outputs));
-
-		while (getline(inp, line))
-		{
-			std::stringstream ssd(line);
-			std::vector<double> row_features(number_features, 0.0);
-			std::vector<double> row_outputs(number_outputs, 0.0);
-			for (int i = 0; i < number_features; i++) {
-				getline(ssd, value, delimiter);
-				row_features[i] = std::stod(value);
-			}
-			for (int i = 0; i < number_outputs; i++) {
-				getline(ssd, value, delimiter);
-				row_outputs[i] = std::stod(value);
-			}
-
-			features.push_back(row_features);
-			outputs.push_back(row_outputs);
-		}
-
-		inp.close();
-
-		logger->log(INFO, "Number of instances = " +to_string(features.size()));
-	}
-
+  logger->log(INFO, "Number of instances = " + to_string(features.size()));
+}
 
 } // namespace ml
--- src/utils/utils.h
+++ src/utils/utils.h
@@ -1,104 +1,101 @@
-#include <algorithm>
-#include <vector>
-#include <chrono>
-#include <random>
-#include <memory>
-#include <unordered_set>
-#include "logging.h"
 #include "json.hpp"
+#include "logging.h"
+#include <memory>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-namespace ml
-{
+namespace ml {
 
-	struct TrainTestData {
-	
-		/**
-		 * Features of training data
-		 */
-		vector<vector<double>> train_features {} ;
+struct TrainTestData {
 
-		/**
-		 * Labels of training data
-		 */
-		vector<vector<double>> train_labels {};
+  /**
+   * Features of training data
+   */
+  vector<vector<double>> train_features{};
 
-		/**
-		 * Features of test data
-		 */
-		vector<vector<double>> test_features {} ;
+  /**
+   * Labels of training data
+   */
+  vector<vector<double>> train_labels{};
 
-		/**
-		 * Labels of training data
-		 */
-		vector<vector<double>> test_labels {};
-	};
+  /**
+   * Features of test data
+   */
+  vector<vector<double>> test_features{};
 
-	class Utilities
-	{
-	public:
+  /**
+   * Labels of training data
+   */
+  vector<vector<double>> test_labels{};
+};
 
-		/**
-		 * Proportion of input data to be used for training
-		 */
-		double train_ratio = 0.75;
+class Utilities {
+public:
+  /**
+   * Proportion of input data to be used for training
+   */
+  double train_ratio = 0.75;
 
-		/**
-		 * Whether the data should be randomly shuffled before splitting into train and test sets.
-		 */
-		bool shuffle_data = true;
+  /**
+   * Whether the data should be randomly shuffled before splitting into train
+   * and test sets.
+   */
+  bool shuffle_data = true;
 
-		/**
-		 * random seed for shuffling data
-		 */
-		int random_seed = 1;
+  /**
+   * random seed for shuffling data
+   */
+  int random_seed = 1;
 
-		/**
-		 * Default constructor for utilities
-		 */
-		Utilities() {};
+  /**
+   * Default constructor for utilities
+   */
+  Utilities() {};
 
-		/**
-		 * Constructor for utilities class
-		 */
-		Utilities(shared_ptr<Logger> logger) ;
-		/**
-		 * Destructor for utilities class
-		 */
-		~Utilities() {};
+  /**
+   * Constructor for utilities class
+   */
+  Utilities(shared_ptr<Logger> logger);
+  /**
+   * Destructor for utilities class
+   */
+  ~Utilities() {};
 
-		/**
-		 * Pointer to a Logger instance
-		 */
-		shared_ptr<Logger> logger;
+  /**
+   * Pointer to a Logger instance
+   */
+  shared_ptr<Logger> logger;
 
-		/** Split the input data for features and outputs into training and test sets.
-		 */
-		TrainTestData train_test_split(const vector<vector<double>> &features, const vector<vector<double>> &outputs);
+  /** Split the input data for features and outputs into training and test sets.
+   */
+  void train_test_split(const vector<vector<double>> &features,
+                        const vector<vector<double>> &outputs,
+                        TrainTestData &train_test);
 
-		/**
-		 * Read data from input file and split it into training and test sets.
-		 */
-		TrainTestData get_train_test_data(nlohmann::json model_parameters);
+  /**
+   * Read data from input file and split it into training and test sets.
+   */
+  TrainTestData get_train_test_data(nlohmann::json model_parameters);
 
+  /**
+   *Read input parameters from a file in JSON format.
+   */
+  nlohmann::json_abi_v3_11_3::json read_json(std::string inputFileName);
 
-		/**
-		 *Read input parameters from a file in JSON format.
-		 */
-		nlohmann::json_abi_v3_11_3::json read_json(std::string inputFileName);
-
-		/**
-		 * Read data for features and outputs from a user-specified data file
-		 * The first line of the file must being with two integers specifying
-		 * the number of columns containing features and outputs respectively.
-		 *
-		 * @param dataFileName: Absolute path to data file.
-		 * @param features: Array to store data for features.
-		 * @param outputs: Array to store data for outputs.
-		 */
-		void read_data(std::string data_file,
-					   std::vector<std::vector<double>> &features,
-					   std::vector<std::vector<double>> &outputs, char delimiter = ',');
-	};
+  /**
+   * Read data for features and outputs from a user-specified data file
+   * The first line of the file must being with two integers specifying
+   * the number of columns containing features and outputs respectively.
+   *
+   * @param dataFileName: Absolute path to data file.
+   * @param features: Array to store data for features.
+   * @param outputs: Array to store data for outputs.
+   */
+  void read_data(std::string data_file,
+                 std::vector<std::vector<double>> &features,
+                 std::vector<std::vector<double>> &outputs,
+                 char delimiter = ',');
+};
 } // namespace ml
