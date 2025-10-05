f13e6cce824e610e2f368092ea98886de31a4160
nagarajankarthik <nkarthik3092@gmail.com>
2025-10-05 16:27:28 +0800

    Added pragma once to prevent duplicate inclusions.

--- src/base_model/base_model.h
+++ src/base_model/base_model.h
@@ -1,113 +1,118 @@
-#include <vector>
-#include <unordered_set>
-#include <algorithm>
-#include <random>
 #include "../utils/json.hpp"
 #include "../utils/logging.h"
+#include <algorithm>
+#include <random>
+#include <unordered_set>
+#include <vector>
+#pragma once
 
 using namespace std;
 
 namespace ml {
 
-	struct DoubleHash {
-	    size_t operator()(double key) const {
-		return std::hash<int64_t>()(static_cast<int64_t>(key * 1000000)); // Scale and hash
-	    }
-	};
+struct DoubleHash {
+  size_t operator()(double key) const {
+    return std::hash<int64_t>()(
+        static_cast<int64_t>(key * 1000000)); // Scale and hash
+  }
+};
 
-	struct DoubleEqual {
-	    bool operator()(double a, double b) const {
-		return std::fabs(a - b) < 1e-6; 
-	    }
-	};
+struct DoubleEqual {
+  bool operator()(double a, double b) const { return std::fabs(a - b) < 1e-6; }
+};
 
+class BaseModel {
 
-	class BaseModel {
+public:
+  /**
+   * random seed
+   */
+  int random_seed = 0;
 
-		public:
+  /**
+   * Random number engine
+   */
+  std::mt19937 random_generator;
 
-		/**
-		 * random seed
-		 */
-		int random_seed = 0;
+  /**
+   * Features for training data
+   */
+  vector<vector<double>> train_features{};
 
-		/**
-		 * Random number engine
-		 */
-		std::mt19937 random_generator;
+  /**
+   * Labels for training data
+   */
+  vector<vector<double>> train_labels{};
 
-		/**
-		 * Features for training data
-		 */
-		vector<vector<double>> train_features {};
+  /**
+   * Logger
+   */
+  shared_ptr<Logger> logger;
 
-		/**
-		 * Labels for training data
-		 */
-		vector<vector<double>> train_labels {};
+  /**
+   * Constructor for base model.
+   */
+  BaseModel(nlohmann::json model_parameters, shared_ptr<Logger> logger);
 
-		/**
-		 * Logger
-		 */
-		shared_ptr<Logger> logger;
+  /**
+   * Destructor for base model
+   */
+  ~BaseModel() {};
 
-		/**
-		 * Constructor for base model.
-		 */
-		BaseModel(nlohmann::json model_parameters, shared_ptr<Logger> logger);
+  /**
+   * Prototype for model training
+   */
+  virtual void fit(const vector<vector<double>> &&features,
+                   const vector<vector<double>> &&labels) = 0;
 
-		/**
-		 * Destructor for base model
-		 */
-		~BaseModel() {} ;
+  /**
+   * Prototype for model inference
+   */
+  virtual vector<vector<double>>
+  predict(const vector<vector<double>> &test_features) = 0;
 
-		/**
-		 * Prototype for model training
-		 */
-		virtual void fit(const vector<vector<double>> && features, const vector<vector<double>> && labels) = 0;
+  /**
+   * Convert elements of 2D array from type double to int.
+   */
+  vector<vector<int>> double_to_int(const vector<vector<double>> &data);
 
-		/**
-		 * Prototype for model inference
-		 */
-		virtual vector<vector<double>> predict(const vector<vector<double>> & test_features) = 0;
+  /**
+   * Get unique entries in each column of an input 2D vector containing
+   * integers.
+   */
+  vector<unordered_set<int>> get_unique_classes(vector<vector<int>> outputs);
 
-		/**
-		 * Convert elements of 2D array from type double to int.
-		 */
-		vector<vector<int>> double_to_int(const vector<vector<double>> & data);
+  /**
+   * Get confusion matrix for a single output variable
+   */
+  vector<vector<int>>
+  get_confusion_matrix_single(const vector<vector<int>> &predictions,
+                              const vector<vector<int>> &test_outputs,
+                              unordered_set<int> unique_classes,
+                              int index_output);
 
-		/**
-		 * Get unique entries in each column of an input 2D vector containing integers.
-		 */
-		vector<unordered_set<int>> get_unique_classes(vector<vector<int>> outputs);
+  /**
+   * Convert 2D array to string for logging purposes
+   */
+  template <class T> string array_2d_to_string(vector<vector<T>> matrix);
 
+  /**
+   * Get confusion matrix for each output variable.
+   */
+  void
+  get_confusion_matrices(const vector<vector<double>> &test_predictions_double,
+                         const vector<vector<double>> &test_labels_double);
 
-		/**
-		 * Get confusion matrix for a single output variable
-		 */
-		vector<vector<int>> get_confusion_matrix_single(const vector<vector<int>> &predictions, const vector<vector<int>> &test_outputs, unordered_set<int> unique_classes, int index_output);
+  /**
+   * Get root mean square error for each output variable.
+   */
+  void get_root_mean_square_error(const vector<vector<int>> &test_predictions,
+                                  const vector<vector<int>> &test_labels);
 
-		/**
-		 * Convert 2D array to string for logging purposes
-		 */
-		template <class T>
-		string array_2d_to_string(vector<vector<T>> matrix);
-
-		/**
-		 * Get confusion matrix for each output variable.
-		 */
-		void get_confusion_matrices(const vector<vector<double>> & test_predictions_double, const vector<vector<double>> & test_labels_double);
-
-		/**
-		 * Get root mean square error for each output variable.
-		 */			
-		void get_root_mean_square_error(const vector<vector<int>> & test_predictions, const vector<vector<int>> & test_labels);
-
-		/**
-		 * Prototype for model evaluation
-		 */
-		virtual void evaluate(const vector<vector<double>> & test_features, const vector<vector<double>> & test_labels) = 0;
-
-
-	};
-}// namespace ml
+  /**
+   * Prototype for model evaluation
+   */
+  virtual void evaluate(const vector<vector<double>> &test_features,
+                        const vector<vector<double>> &test_labels) = 0;
+};
+} // namespace ml
