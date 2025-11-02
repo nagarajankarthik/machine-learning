807c692fe9fd0ed9aceb5df8ab891c2b6f70660e
nagarajankarthik <nkarthik3092@gmail.com>
2025-11-02 21:54:22 +0800

    Added logging statement before call to function which reads binary data.

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -99,6 +99,8 @@ TrainTestData Utilities::get_train_test_data(nlohmann::json model_parameters) {
       read_data_csv(test_data_path, number_features, number_outputs,
                     train_test.test_features, train_test.test_labels);
     } else if (data_format == "bin") {
+      logger->log(INFO, "Reading training data in binary format from " +
+                            train_data_path);
       read_data_bin(train_data_path, number_features, number_outputs,
                     train_test.train_features, train_test.train_labels);
       read_data_bin(test_data_path, number_features, number_outputs,
